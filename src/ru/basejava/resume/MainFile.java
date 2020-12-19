d29e077dc98510549c0f18c2214d1c68cdfa92ed
avasaris <artvas@yandex.ru>
2020-12-19 18:48:23 +0200

    lesson08.  Directory recursion.

--- src/ru/basejava/resume/MainFile.java
+++ src/ru/basejava/resume/MainFile.java
@@ -5,7 +5,20 @@ import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) throws IOException {
-        File file = new File("./.gitignore");
-        System.out.println(file.getCanonicalPath());
+        printDir("./");
+    }
+
+    private static void printDir(String path) throws IOException {
+        File dir = new File(path);
+        if(dir.isDirectory()){
+            String[] list = dir.list();
+            if(list == null) return;
+            for(String name: list){
+                if(!name.equals(".git") && !name.equals(".idea")) {
+                    printDir(path + "/" + name);
+                }
+            }
+        }
+        System.out.println(dir.getCanonicalPath());
     }
 }
